c3c01d3c1f347c0ed68cb52d0e82532b697b3e88
RafaelCamposTXR <102463380+RafaelCamposTXR@users.noreply.github.com>
2024-03-24 18:23:23 -0300

    Update NavBar.jsx

--- lifegold/src/components/NavBar/NavBar.jsx
+++ lifegold/src/components/NavBar/NavBar.jsx
@@ -17,27 +17,5 @@ class NavBar extends React.Component {
   }
 }
 
-const styles = {
-  menu: {
-    backgroundColor: '#333',
-    padding: '10px 0',
-    display: 'flex',
-    justifyContent: 'flex-end',
-  },
-  listaMenu: {
-    listStyleType: 'none',
-    margin: 25,
-    padding: 0,
-    display: 'flex',
-  },
-  itemMenu: {
-    marginLeft: '20px',
-  },
-  link: {
-    color: '#fff',
-    textDecoration: 'none',
-    fontWeight: 'bold',
-  }
-};
 
 export default NavBar;
