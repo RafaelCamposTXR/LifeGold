517a2068ca28f5db3c8806e51740d39fd2ee6802
RafaelCamposTXR <102463380+RafaelCamposTXR@users.noreply.github.com>
2024-03-24 20:37:23 -0300

    melhoria na barra de navegação

--- lifegold/src/components/NavBar/NavBar.jsx
+++ lifegold/src/components/NavBar/NavBar.jsx
@@ -7,8 +7,7 @@ function NavBar() {
   return (
     <div className={style.menu}>
       <ul className={style.listaMenu}>
-        <li><Link to="/" className={style.botaoNavegacao}>Home</Link></li>
-        <li><Link to="/sobre" className={style.botaoNavegacao}>Sobre</Link></li>
+        <li><Link to="/" className={style.botaoNavegacao}>Sobre</Link></li>
         <li><Link to="/piscinas" className={style.botaoNavegacao}>Piscinas</Link></li>
         <li><Link to="/produtos" className={style.botaoNavegacao}>Produtos</Link></li>
         <li><Link to="/descubra" className={style.botaoNavegacao}>Descubra</Link></li>
