f79b817da45008e7341a76393735cbeeae51fea1
RafaelCamposTXR <102463380+RafaelCamposTXR@users.noreply.github.com>
2024-03-25 18:20:22 -0300

    alterando ordem de itens na NavBar

--- lifegold/src/components/NavBar/NavBar.jsx
+++ lifegold/src/components/NavBar/NavBar.jsx
@@ -10,8 +10,8 @@ function NavBar() {
     <div className={style.menu}>
       <ul className={style.listaMenu}>
         <li><Link to="/" className={location.pathname === '/' ? style.botaoNavegacaoAtivo : style.botaoNavegacao}>SOBRE</Link></li>
-        <li><Link to="/produtos" className={location.pathname === '/produtos' ? style.botaoNavegacaoAtivo : style.botaoNavegacao}>PRODUTOS</Link></li>
         <li><Link to="/piscinas" className={location.pathname === '/piscinas' ? style.botaoNavegacaoAtivo : style.botaoNavegacao}>PISCINAS</Link></li>
+        <li><Link to="/produtos" className={location.pathname === '/produtos' ? style.botaoNavegacaoAtivo : style.botaoNavegacao}>PRODUTOS</Link></li>
         <li><Link to="/descubra" className={location.pathname === '/descubra' ? style.botaoNavegacaoAtivo : style.botaoNavegacao}>DESCUBRA</Link></li>
         <li><Link to="/contato" className={location.pathname === '/contato' ? style.botaoNavegacaoAtivo : style.botaoNavegacao}>CONTATO</Link></li>
       </ul>
