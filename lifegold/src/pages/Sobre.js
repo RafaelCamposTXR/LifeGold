a9eea91057fb8a8e5076e9678397b98defe772e7
RafaelCamposTXR <102463380+RafaelCamposTXR@users.noreply.github.com>
2024-03-25 13:45:34 -0300

    adicionando aba de marketing

--- lifegold/src/pages/Sobre.js
+++ lifegold/src/pages/Sobre.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react';
 import logo from '../components/media/central.png';
+import porqueGeral from '../components/media/porquegeral.png';
+import texto from '../components/media/texto.png';
 import '../components/styles.css'
 
 function Sobre() {
@@ -7,11 +9,10 @@ function Sobre() {
         <div className="Sobre">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                Texto Inicial do site
-                </p>
             </header>
-        
+            <div className ="App">
+              <img src={porqueGeral} className="App-logo" alt="logo"/>
+            </div>
         </div>
     );
 }
