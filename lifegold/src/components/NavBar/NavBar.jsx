c660985f69fc690ec5f4da454b4414e14a676899
EymiM <43094416+EymiM@users.noreply.github.com>
2024-03-25 02:06:59 -0300

    Atualizando a barra de navegação

--- lifegold/src/components/NavBar/NavBar.jsx
+++ lifegold/src/components/NavBar/NavBar.jsx
@@ -1,17 +1,19 @@
 // NavBar.js
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import style from './NavBar.module.css';
 
 function NavBar() {
+  const location = useLocation();
+
   return (
     <div className={style.menu}>
       <ul className={style.listaMenu}>
-        <li><Link to="/" className={style.botaoNavegacao}>Sobre</Link></li>
-        <li><Link to="/produtos" className={style.botaoNavegacao}>Produtos</Link></li>
-        <li><Link to="/piscinas" className={style.botaoNavegacao}>Piscinas</Link></li>
-        <li><Link to="/descubra" className={style.botaoNavegacao}>Descubra</Link></li>
-        <li><Link to="/contato" className={style.botaoNavegacao}>Contato</Link></li>
+        <li><Link to="/" className={location.pathname === '/' ? style.botaoNavegacaoAtivo : style.botaoNavegacao}>SOBRE</Link></li>
+        <li><Link to="/produtos" className={location.pathname === '/produtos' ? style.botaoNavegacaoAtivo : style.botaoNavegacao}>PRODUTOS</Link></li>
+        <li><Link to="/piscinas" className={location.pathname === '/piscinas' ? style.botaoNavegacaoAtivo : style.botaoNavegacao}>PISCINAS</Link></li>
+        <li><Link to="/descubra" className={location.pathname === '/descubra' ? style.botaoNavegacaoAtivo : style.botaoNavegacao}>DESCUBRA</Link></li>
+        <li><Link to="/contato" className={location.pathname === '/contato' ? style.botaoNavegacaoAtivo : style.botaoNavegacao}>CONTATO</Link></li>
       </ul>
     </div>
   );
