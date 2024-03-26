dddf1ecfa38a745001b489c69b1d069cfb106562
RafaelCamposTXR <102463380+RafaelCamposTXR@users.noreply.github.com>
2024-03-25 21:41:35 -0300

    inserindo logo na tela de contato

--- lifegold/src/components/TelaContato/TelaContato.jsx
+++ lifegold/src/components/TelaContato/TelaContato.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import style from './TelaContato.module.css';
 import img from '../media/fundoTelaContato.png';
+import logo from '../media/LifeGoldlogo.png';
 
 function TelaContato() {
   const location = useLocation();
 
   return(
     <div className= {style.contato} >
-        <img src={img} className={style.fundo}/>
+      <div className= {style.listMenu}>
+        Entre em Contato
+      </div>
+      <img src={logo} className={style.logo}/>
+      <img src={img} className={style.fundo}/>
     </div>
   );
 }
