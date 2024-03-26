f2fa9306079aa838b8f56bdfc7b4f8944e39b3b0
RafaelCamposTXR <102463380+RafaelCamposTXR@users.noreply.github.com>
2024-03-26 14:57:09 -0300

    adicionando itens ao catálogo

--- lifegold/src/components/MenuItens/Menu.js
+++ lifegold/src/components/MenuItens/Menu.js
@@ -1,38 +1,30 @@
 import React from 'react';
 import './MenuItens.css';
 import MenuItem from './MenuItem.js';
-import inseticida from '../media/inseticida.png'
-import aspirador from '../media/aspirador.jpg'
 
 function Menu() {
   const menuItems = [
-    { name: 'Inseticida', price: 2.49, image: inseticida },
-    { name: 'ASPIRADOR', price: 140, image: aspirador },
-    { name: 'outra coisa', price: 1.99, image: inseticida },
-    { name: 'Inseticida', price: 5.99, image: inseticida },
-    { name: 'outra coisa', price: 2.49, image: inseticida },
-    { name: 'outra coisa', price: 1.99, image: inseticida },
-    { name: 'Inseticida', price: 5.99, image: inseticida },
-    { name: 'outra coisa', price: 2.49, image: inseticida },
-    { name: 'outra coisa', price: 1.99, image: inseticida },
-    { name: 'Inseticida', price: 5.99, image: inseticida },
-    { name: 'outra coisa', price: 2.49, image: inseticida },
-    { name: 'outra coisa', price: 1.99, image: inseticida },
-    { name: 'Inseticida', price: 5.99, image: inseticida },
-    { name: 'outra coisa', price: 2.49, image: inseticida },
-    { name: 'outra coisa', price: 1.99, image: inseticida },
-    { name: 'Inseticida', price: 5.99, image: inseticida },
-    { name: 'outra coisa', price: 2.49, image: inseticida },
-    { name: 'outra coisa', price: 1.99, image: inseticida },
-    { name: 'Inseticida', price: 5.99, image: inseticida },
-    { name: 'outra coisa', price: 2.49, image: inseticida },
-    { name: 'outra coisa', price: 1.99, image: inseticida },
-    { name: 'Inseticida', price: 5.99, image: inseticida },
-    { name: 'outra coisa', price: 2.49, image: inseticida },
-    { name: 'outra coisa', price: 1.99, image: inseticida },
-    { name: 'Inseticida', price: 5.99, image: inseticida },
-    { name: 'outra coisa', price: 2.49, image: inseticida },
-    { name: 'outra coisa', price: 1.99, image: inseticida },
+    { name: 'PENEIRA CATA-FOLHAS', image: require('../media/peneira.jpg')},
+    { name: 'ASPIRADOR', image:require('../media/aspirador.jpg')},
+    { name: 'ADAPTADOR 1/2', image: require('../media/adaptador.jpg')},
+    { name: 'BOMBA 1/2 CV CONJUNTO', image: require('../media/bombaVelcro1_2.jpg')},
+    { name: 'BOMBA 1/3 CV CONJUNTO', image: require('../media/bombaVelcro1_3.jpg')},
+    { name: 'CLORADOR FLUTUANTE MARGARIDA', image: require('../media/cloradorFlutuante.jpg')},
+    { name: 'ESTOJO TESTE', image: require('../media/estojoTeste.jpg')},
+    { name: 'FILTRO VELCRO V40', image: require('../media/filtroVelcrov40.jpg')},
+    { name: 'FILTRO VELCRO V30', image: require('../media/filtroVelcrov30.jpg')},
+    { name: 'MANGUEIRA FLUTUANTE 1/2', image: require('../media/mangueiraFlutuante1_2.jpg')},
+    { name: 'PONTEIRA P/ MANGUEIRA', image: require('../media/ponteira.jpg')},
+    { name: 'CLORO 3 EM 1', image: require('../media/cloro.jpg')},
+    { name: 'SULFATO DE ALUMÍNIO 2KG', image: require('../media/sulfato.jpg')},
+    { name: 'ELEVADOR DE pH', image: require('../media/barrilha.jpg')},
+    { name: 'CLARIFICANTE', image: require('../media/g_clarificante.jpg')},
+    { name: 'ALGICIDA DE MANUTENÇÃO', image: require('../media/g_algicidamanutencao.jpg')},
+    { name: 'ALGICIDA DE CHOQUE', image: require('../media/g_algicidachoque.jpg')},
+    { name: 'LIMPA BORDAS', image: require('../media/g_limpabordas.jpg')},
+    { name: 'SUPER TRATAMENTO SEMANAL', image: require('../media/g_tratamentosemanal.jpg')},
+    { name: 'TABLETE T200', image: require('../media/g_t200.jpg')},
+    { name: 'BALDE CLORO GRANULADO 10KG', image: require('../media/g_baldecloro.jpg')},
   ];
 
   return (
--- lifegold/src/components/MenuItens/MenuItem.js
+++ lifegold/src/components/MenuItens/MenuItem.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import './MenuItens.css';
 
-function MenuItem({ name, price, image}) {
+function MenuItem({ name, image}) {
   return (
     <div className="item-menu">
       <img src={image} className="image" alt=""/>
       <div className="nome-item">
         {name}
       </div>
-      <div className="preco-item">
-        R${price}
-      </div>
+
       
     </div>
   );
