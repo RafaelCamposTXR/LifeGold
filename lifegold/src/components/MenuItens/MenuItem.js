764ac8914dbe1f8d94d3ef0ab93885dc195aa03d
RafaelCamposTXR <102463380+RafaelCamposTXR@users.noreply.github.com>
2024-03-25 23:02:17 -0300

    criando diretório css próprio para catálogo de itens

--- lifegold/src/components/MenuItens/MenuItem.js
+++ lifegold/src/components/MenuItens/MenuItem.js
@@ -1,22 +1,15 @@
 import React from 'react';
-import '../styles.css';
+import './MenuItens.css';
 
 function MenuItem({ name, price, image}) {
   return (
     <div className="item-menu">
-      <div className="nome-item">{name}</div>
-      <div className="preco-item">${price}</div>
-      <br>
-      </br>
-      <br>
-      </br>
-      <br>
-      </br>
-      <br>
-      </br>
-      <br>
-      </br>
-      <img src={image} className="image"/>
+      <div className="nome-item">
+        {name}
+        {price}
+        <img src={image} className="image"/>
+      </div>
+      
     </div>
   );
 }
