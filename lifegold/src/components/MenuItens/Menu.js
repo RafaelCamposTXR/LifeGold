5c91ac87b6f8ff34264e72b93a5651bb84264624
RafaelCamposTXR <102463380+RafaelCamposTXR@users.noreply.github.com>
2024-03-25 22:54:30 -0300

    inserindo imagem de produto

--- lifegold/src/components/MenuItens/Menu.js
+++ lifegold/src/components/MenuItens/Menu.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import '../styles.css';
 import MenuItem from './MenuItem.js';
+import inseticida from '../media/inseticida.png'
 
 function Menu() {
   const menuItems = [
-    { name: 'Inseticida', price: 5.99 },
-    { name: 'outra coisa', price: 2.49 },
-    { name: 'outra coisa', price: 1.99 },
+    { name: 'Inseticida', price: 5.99, image: inseticida },
+    { name: 'outra coisa', price: 2.49, image: inseticida },
+    { name: 'outra coisa', price: 1.99, image: inseticida },
   ];
 
   return (
     <div className="menu">
       <div className="lista-menu">
         {menuItems.map((item, index) => (
-          <MenuItem key={index} name={item.name} price={item.price} />
+          <MenuItem key={index} name={item.name} price={item.price} image ={item.image} />
         ))}
       </div>
     </div>
--- lifegold/src/components/MenuItens/MenuItem.js
+++ lifegold/src/components/MenuItens/MenuItem.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import '../styles.css';
 
-function MenuItem({ name, price }) {
+function MenuItem({ name, price, image}) {
   return (
     <div className="item-menu">
       <div className="nome-item">{name}</div>
       <div className="preco-item">${price}</div>
+      <br>
+      </br>
+      <br>
+      </br>
+      <br>
+      </br>
+      <br>
+      </br>
+      <br>
+      </br>
+      <img src={image} className="image"/>
     </div>
   );
 }
