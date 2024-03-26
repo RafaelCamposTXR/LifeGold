aef7defe6e5e3582606270d3d7f7cb1ef7c92d60
RafaelCamposTXR <102463380+RafaelCamposTXR@users.noreply.github.com>
2024-03-26 10:25:34 -0300

    adicionando itens

--- lifegold/src/components/MenuItens/MenuItem.js
+++ lifegold/src/components/MenuItens/MenuItem.js
@@ -4,9 +4,11 @@ import './MenuItens.css';
 function MenuItem({ name, price, image}) {
   return (
     <div className="item-menu">
-      <img src={image} className="image"/>
+      <img src={image} className="image" alt=""/>
       <div className="nome-item">
         {name}
+      </div>
+      <div className="preco-item">
         {price}
       </div>
       
