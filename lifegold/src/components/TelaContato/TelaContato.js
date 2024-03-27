3e06d51ddc39729bf64e078cb7416777de7bcb70
EymiM <43094416+EymiM@users.noreply.github.com>
2024-03-27 13:10:47 -0300

    atualizando contato

--- lifegold/src/components/TelaContato/TelaContato.js
+++ lifegold/src/components/TelaContato/TelaContato.js
@@ -8,13 +8,24 @@ function TelaContato() {
   const location = useLocation();
   return(
     <div className= "contato" >
+
       <div className= "listaMenu">
-      <div className= "texto">
+        <div className= "texto">
           ENTRE EM CONTATO
         </div>
+
+        <div className='texto-normal' style={{ whiteSpace: 'pre-line' }} >
+          @lifegoldpiscinas {'\n'}
+          (34) 3612-5203  /  (34) 99803-2462 {'\n'}
+          lifegoldpiscinas@gmail.com
+        </div>
+
       </div>
+      
       <img src={logo} className="logo"/>
+      
       <img src={img} className="fundo"/>
+    
     </div>
   );
 }
