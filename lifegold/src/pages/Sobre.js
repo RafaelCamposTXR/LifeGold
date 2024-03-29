a714775919374b164a327f04cca51e0d4b869d36
RafaelCamposTXR <102463380+RafaelCamposTXR@users.noreply.github.com>
2024-03-29 13:38:35 -0300

    Update Sobre.js

--- lifegold/src/pages/Sobre.js
+++ lifegold/src/pages/Sobre.js
@@ -15,6 +15,7 @@ function Sobre() {
             <img src={logo} className="App-logo" alt="logo" />
             <img src={fundoCentral} className="App-logo" alt="logo" />
             <img src={porqueGeralPC} className="PorqueGeralPC" alt="logo"/>
+            <BubbleContainer numBubbles={10} areaWidth={1400} areaHeight={900} />
             <img src={porqueGeralMobile} className="PorqueGeralMobile" alt="logo"/>
               <div className='texto-menor'>
                 <p>Fundada por Tiago Augusto, profissional com mais de 20 anos de experiência na área de construção e venda de piscinas, a LifeGold já produziu e vendeu mais de 300 piscinas. Acompanhamos você do início ao fim da construção, para que você possa ter profissionais qualificados e certificados na área, sempre à disposição para tornar seu sonho realidade</p>
@@ -22,8 +23,7 @@ function Sobre() {
               <img src={IndicaPiscina} className="PorqueGeralPC" alt="logo"/>
             </header>
             <div className ="App">
-              <img src={porqueGeralPC} className="App-logo" alt="logo"/>
-              <BubbleContainer numBubbles={10} areaWidth={1400} areaHeight={900} />
+              
             </div>
             <TelaContato/>
         </div>
