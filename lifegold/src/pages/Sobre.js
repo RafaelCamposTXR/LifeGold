ecbbe418287a298ad6b01f4fe0c74f83b92b0d20
RafaelCamposTXR <102463380+RafaelCamposTXR@users.noreply.github.com>
2024-03-29 13:48:04 -0300

    adicionando IrPiscinas

--- lifegold/src/pages/Sobre.js
+++ lifegold/src/pages/Sobre.js
@@ -4,6 +4,7 @@ import fundoCentral from '../components/media/FundoSubtitulo.png';
 import porqueGeralPC from '../components/media/PorqueGeralPC.png';
 import porqueGeralMobile from '../components/media/PorqueGeralMobile.png';
 import IndicaPiscina from '../components/media/IndicaPiscina.png';
+import IrPiscina from '../components/media/IrPiscinas.png';
 import '../components/styles.css';
 import TelaContato from '../components/TelaContato/TelaContato';
 import BubbleContainer from '../components/Bolhas/BubbleContainer';
@@ -21,6 +22,7 @@ function Sobre() {
                 <p>Fundada por Tiago Augusto, profissional com mais de 20 anos de experiência na área de construção e venda de piscinas, a LifeGold já produziu e vendeu mais de 300 piscinas. Acompanhamos você do início ao fim da construção, para que você possa ter profissionais qualificados e certificados na área, sempre à disposição para tornar seu sonho realidade</p>
               </div>
               <img src={IndicaPiscina} className="PorqueGeralPC" alt="logo"/>
+              <img src={IrPiscina} className="PorqueGeralPC" alt="logo"/>
             </header>
             <div className ="App">
               
